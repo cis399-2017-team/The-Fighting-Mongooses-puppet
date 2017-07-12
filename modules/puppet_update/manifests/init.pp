45f6edb1216dbc7cbedb783f1ce1ec2b1c0ba50c
Joseph Shea-Bianco <jsheabia@uoregon.edu>
2017-07-12 21:00:40 +0000

    Removed puppet_update module, added cron task to manifests

--- modules/puppet_update/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-# update /etc/puppet on puppet master (use puppet master node name)
-node ip-10-0-x-y {
-	cron { "puppet update":
-		command => "cd /etc/puppet && git pull -q origin master",
-		user    => root,
-		minute  => "*/5",
-	}
-}
