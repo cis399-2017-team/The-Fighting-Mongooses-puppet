a390634a2a3c4dfcc29ed882d3ab6eb16720d2a1
Joseph Shea-Bianco <j.sheabianco@gmail.com>
2017-07-19 13:07:05 -0700

    Moved from php to pure html

--- modules/apache/files/html/bottom.php
+++
@@ -1,6 +0,0 @@
-</div>
-<div id="footer">
-	<span id="footertext">The Fighting Mongooses<br>Website by Joseph Shea-Bianco for CIS 399, Summer 2017.</span>
-</div>
-</body>
-</html>
--- modules/apache/files/html/index.php
+++
@@ -1,3 +0,0 @@
-<?php $title = "Fighting Mongooses"; include("top.php"); ?>
-	<iframe id="videoClip" width="560" height="315" src="https://www.youtube.com/embed/hxh-YVSoZeE" frameborder="0" allowfullscreen></iframe>
-<?php include("bottom.php"); ?>
