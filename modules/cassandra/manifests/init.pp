650ed2359b192709ab75078c4147cdf8543ec407
Joseph Shea-Bianco <jsheabia@uoregon.edu>
2017-08-16 14:52:30 -0700

    Commented out one of the execs

--- modules/cassandra/manifests/init.pp
+++ modules/cassandra/manifests/init.pp
@@ -39,11 +39,11 @@ class cassandra {
 		subscribe => File["/etc/cassandra/cassandra.yaml"]
 	}
 
-	exec { "python patch":
-		path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-		command => "pip install cassandra-driver && export CQLSH_NO_BUNDLED=TRUE",
-		onlyif => "if [ \"$CQLSH_NO_BUNDLED\" == TRUE ]; then true; else false; fi", 
-		require => [ Package["python-pip"], Package["cassandra"] ],
-	}
+#	exec { "python patch":
+#		path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+#		command => "pip install cassandra-driver && export CQLSH_NO_BUNDLED=TRUE",
+#		onlyif => "if [ \"$CQLSH_NO_BUNDLED\" == TRUE ]; then true; else false; fi", 
+#		require => [ Package["python-pip"], Package["cassandra"] ],
+#	}
 }
 
